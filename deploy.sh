90e221367fe3f0aeac2811bf9a95ceb0e0ae93c2
张伟 <myzony@qq.com>
2021-07-29 21:01:31 +0800

    commit

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,6 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-# npm run docs:build
-
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-echo 'www.ms.58taoweb.com' > CNAME
-
 git add -A
 git commit -m 'travis ci'
 
