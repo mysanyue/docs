a26d396f398e5e061347de73f2322da7f0237485
张伟 <myzony@qq.com>
2021-10-09 22:46:24 +0800

    commit 2021-10-9

--- docs/selfExamin/sidebar.js
+++ docs/selfExamin/sidebar.js
@@ -2,6 +2,7 @@ module.exports = [
   {
     title: '自学考试',
     collapsable: true,
+    sidebarDepth: 2,
     children: [
       ['/selfExamin/resourceCon', '信息资源建设'],
       ['/selfExamin/informationCon', '信息咨询'],
