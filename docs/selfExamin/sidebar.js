6a055045c7c49c1fc2ea069fdb780016c238fdae
三月风 <myzony@qq.com>
2022-10-19 22:53:41 +0800

    feat: 2022-10-19

--- docs/selfExamin/sidebar.js
+++ docs/selfExamin/sidebar.js
@@ -5,6 +5,8 @@ module.exports = [
     sidebarDepth: 2,
     children: [
       ['/selfExamin/2020', '信息资源建设 2020 年'],
+      ['/selfExamin/2019', '信息资源建设 2019 年'],
+      ['/selfExamin/2018', '信息资源建设 2018 年'],
       ['/selfExamin/2017', '信息资源建设 2017 年'],
     ],
   },
